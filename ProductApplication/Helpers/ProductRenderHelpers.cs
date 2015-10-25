4423fd0e511d819f0077782fb9e3a3666765b8ff
Emir Osmanoski <emir.osmanoski@gmail.com>
2015-10-25 22:15:38 +0100

    Move stock color to HTML Helper

---
+++ ProductApplication/Helpers/ProductRenderHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace ProductApplication.Helpers
+{
+    public static class ProductRenderHelpers
+    {
+        public static string StockColor(this HtmlHelper helper, int stockCount)
+        {
+            string quantityClass = "warning-stock";
+
+            if (stockCount >= 10)
+            {
+                quantityClass = "in-stock";
+            }
+            else if (stockCount < 5)
+            {
+                quantityClass = "critical-stock";
+            }
+            
+            return quantityClass;
+        }
+    }
+}
